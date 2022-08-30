e016c81dc976428f7dc9b4984fd0365c7171ec11
OJWatson <oj.watson@hotmail.co.uk>
2022-08-30 16:51:18 +0100

    samrat_read_params checks

--- R/parameters.R
+++ R/parameters.R
@@ -16,6 +16,15 @@
 #'       }
 #'
 #' @export
+#' @examples {
+#'
+#' param_file <- system.file(
+#' "extdata/som_analysis_parameters.xlsx", package="samrat"
+#' )
+#' pars <- samrat_read_params(param_file)
+#' names(pars)
+#'
+#' }
 samrat_read_params <- function(param_file) {
 
   # check file is suitable name and exists
@@ -45,9 +54,10 @@ samrat_read_params <- function(param_file) {
   cf_pars <- as.data.frame(cf_pars)
 
   #...................................
-  ## Sensitivity analysis parameters
-  sens_pars <- readxl::read_excel(param_file, sheet = "sensitivity_parameters")
-  sens_pars <- as.data.frame(sens_pars)
+  ## TODO: Should this be read in here: Probably no
+  # Sensitivity analysis parameters
+  # sens_pars <- readxl::read_excel(param_file, sheet = "sensitivity_parameters")
+  # sens_pars <- as.data.frame(sens_pars)
 
   # Bring all the parameters into one list
   pars <- list(
@@ -57,5 +67,39 @@ samrat_read_params <- function(param_file) {
   )
 
   return(pars)
+
+}
+
+#------------------------------------------------
+#' check_param_file
+#'
+#' \code{check_param_file} Checks param file is correctly structures
+#'
+#' @param pars Output of [samrat_read_params]
+#'
+#' @keywords internal
+
+check_param_file <- function(pars) {
+
+  # read in the example params from the package
+  param_file <- system.file(
+    "extdata/som_analysis_parameters.xlsx", package="samrat"
+  )
+  pars_ex <- samrat_read_params(param_file)
+
+  # simple check that the names are the same in gen_pars
+  if (!all(names(pars_ex$gen_pars) %in% names(pars$gen_pars))) {
+    stop("param_file general_parameters sheet is missing variables")
+  }
+
+  # simple check that the names are the same in var_pars
+  if (!all(names(pars_ex$var_pars) %in% names(pars$var_pars))) {
+    stop("param_file predictor_parameters sheet is missing variables")
+  }
+
+  # simple check that the names are the same in cf_pars
+  if (!all(names(pars_ex$cf_pars) %in% names(pars$cf_pars))) {
+    stop("param_file counterfactual_parameters sheet is missing variables")
+  }
 
 }
