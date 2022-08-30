6fbcab985ba16a937a222d38750f1d810ce3e4a6
OJWatson <oj.watson@hotmail.co.uk>
2022-08-30 16:52:04 +0100

    f_liv for livelihood strings

---
+++ R/smals.R
@@ -0,0 +1,61 @@
+#' \code{f_liv}
+#' @description  Function to standardise livelihood type nomenclature
+#'
+#' @param liv_term Livelihood liv_term being matched with
+#' @param liv_ss Livelihood substrings
+#' @keywords internal
+
+f_liv <- function(liv_term, liv_ss) {
+
+  # Livelihood
+  if (grepl(paste(liv_ss$livelihood, collapse = "|"), liv_term)) {
+    return(paste(names(livelihood_substrings)[1]))
+  }
+
+  # Agriculturalists
+  if (grepl(paste(liv_ss$agriculturalists, collapse = "|"), liv_term)) {
+    return(paste(names(livelihood_substrings)[2]))
+  }
+
+  # Pastoralists (slightly different to avoid confusion with agropastoralists)
+  if (grepl(paste(liv_ss$pastoralists, collapse = "|"), liv_term) &&
+      !grepl(paste(liv_ss$agropastoralists, collapse = "|"), liv_term)) {
+    return(paste(names(livelihood_substrings)[3]))
+  }
+
+  # Agropastoralists
+  if (grepl(paste(liv_ss$agropastoralists, collapse = "|"), liv_term)) {
+    return(paste(names(livelihood_substrings)[4]))
+  }
+
+  # Riverine
+  if (grepl(paste(liv_ss$riverine, collapse = "|"), liv_term)) {
+    return(paste(names(livelihood_substrings)[5]))
+  }
+
+  # Fishing
+  if (grepl(paste(liv_ss$fishing, collapse = "|"), liv_term)) {
+    return(paste(names(livelihood_substrings)[6]))
+  }
+
+  # Urban
+  if (grepl(paste(liv_ss$urban, collapse = "|"), liv_term)) {
+    return(paste(names(livelihood_substrings)[7]))
+  }
+
+  # Displaced
+  if (grepl(paste(liv_ss$displaced, collapse = "|"), liv_term)) {
+    return(paste(names(livelihood_substrings)[8]))
+  }
+
+  # Refugee
+  if (grepl(paste(liv_ss$refugee, collapse = "|"), liv_term)) {
+    return(paste(names(livelihood_substrings)[9]))
+  }
+
+  # Otherwise return NA
+  if (!grepl(paste(unlist(liv_ss), collapse = "|"), liv_term)) {
+    return(NA)
+  }
+
+}
---
+++ tests/testthat/test-smals.R
@@ -0,0 +1,18 @@
+context("smals")
+
+test_that("f_liv works", {
+
+  lv_terms <- lapply(livelihood_substrings, "[[", 1)
+
+  expect_equal(f_liv(lv_terms[[1]], livelihood_substrings), names(lv_terms)[1])
+  expect_equal(f_liv(lv_terms[[2]], livelihood_substrings), names(lv_terms)[2])
+  expect_equal(f_liv(lv_terms[[3]], livelihood_substrings), names(lv_terms)[3])
+  expect_equal(f_liv(lv_terms[[4]], livelihood_substrings), names(lv_terms)[4])
+  expect_equal(f_liv(lv_terms[[5]], livelihood_substrings), names(lv_terms)[5])
+  expect_equal(f_liv(lv_terms[[6]], livelihood_substrings), names(lv_terms)[6])
+  expect_equal(f_liv(lv_terms[[7]], livelihood_substrings), names(lv_terms)[7])
+  expect_equal(f_liv(lv_terms[[8]], livelihood_substrings), names(lv_terms)[8])
+  expect_equal(f_liv(lv_terms[[9]], livelihood_substrings), names(lv_terms)[9])
+  expect_equal(f_liv("zxc", livelihood_substrings), NA)
+
+})
