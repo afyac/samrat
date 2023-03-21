9093a92f53a552b4b4e3cbbf4739879a9014f08c
truenomad <mohamedayusuf87@gmail.com>
2023-03-21 11:05:16 +0300

    Created the robust_se() function along with its own test cases using the testthat package.

---
+++ R/robust_se.R
@@ -0,0 +1,66 @@
+#' @title Robust Standard Errors
+#'
+#' @description This function calculates robust standard errors for model coefficients, given a robust variance-covariance matrix.
+#'
+#' @param model_fit The fitted model object.
+#' @param varcov The robust variance-covariance matrix.
+#' @param scale A character string indicating the scale of the model coefficients.
+#' Possible values are "linear" (default) or "log".
+#' @return A data frame with the following columns:
+#' \itemize{
+#' \item Rate ratio: The estimate of the model coefficient on the specified scale.
+#' \item 95%CI - lower: The lower bound of the 95% confidence interval for the estimate.
+#' \item 95%CI - upper: The upper bound of the 95% confidence interval for the estimate.
+#' \item Pr(>|z|): The p-value for the significance of the estimate.
+#' }
+#' @examples
+#' library(sandwich)
+#' data(mtcars)
+#'
+#' # Build model
+#' model_fit <- lm(mpg ~ cyl + disp, data = mtcars)
+#'
+#' # Calculate the robust variance-covariance matrix
+#' varcov <- vcovHC(model_fit, type = "HC")
+#'
+#' # Estimate robust standard errors
+#' robust_se(model_fit, varcov, scale = "linear")
+#' @importFrom stats diag pnorm round
+#' @importFrom stats exp coef
+#' @importFrom base ifelse
+#' @export
+
+
+robust_se <- function(model_fit, varcov, scale) {
+  # Check for negative variances
+  if (any(diag(varcov) < 0)) {
+    stop("Negative variance detected")
+  }
+
+  # Calculate robust standard errors
+  std.err <- sqrt(diag(varcov))
+
+  # Calculate rate ratio and confidence intervals
+  if (scale == "log") {
+    rate_ratio <- round(coef(model_fit), 3)
+    ci_lower <- round(coef(model_fit) - 1.96 * std.err, 3)
+    ci_upper <- round(coef(model_fit) + 1.96 * std.err, 3)
+  } else {
+    rate_ratio <- round(exp(coef(model_fit)), 3)
+    ci_lower <- round(exp(coef(model_fit) - 1.96 * std.err), 3)
+    ci_upper <- round(exp(coef(model_fit) + 1.96 * std.err), 3)
+  }
+
+  # Calculate p-value for significance of estimate
+  p_value <- round(2 * pnorm(abs(coef(model_fit) / std.err), lower.tail = FALSE), 3)
+
+  # Create output data frame
+  r_est <- data.frame(rate_ratio, ci_lower, ci_upper, p_value) %>%
+    rename(
+      "Rate ratio" = rate_ratio, "Pr(>|z|)" = p_value,
+      "95%CI - lower" = ci_lower, "95%CI - upper" = ci_upper
+    )
+
+  # Return output
+  return(r_est)
+}
---
+++ tests/testthat/test-robust_se.R
@@ -0,0 +1,72 @@
+context("Test robust_se function")
+
+# Dependencies and model set up -------------------------------------------
+
+library(testthat)
+library(lmtest)
+library(sandwich)
+
+# Load the required data
+data(mtcars)
+
+# Build model
+model_fit <- lm(mpg ~ cyl + disp, data = mtcars)
+
+# Calculate the robust variance-covariance matrix
+varcov <- vcovHC(model_fit, type = "HC")
+
+# Tests -------------------------------------------------------------------
+
+
+# Test case 1: Test the output data frame has correct column names
+test_that(
+  "robust_se returns a data frame with correct column names",
+  {
+    result <- robust_se(model_fit, varcov, scale = "linear")
+    expect_named(result, c("Rate ratio", "95%CI - lower", "95%CI - upper", "Pr(>|z|)"))
+  }
+)
+
+# Test case 2: Test the output data frame has the correct number of rows
+test_that(
+  "robust_se returns a data frame with the correct number of rows",
+  {
+    result <- robust_se(model_fit, varcov, scale = "linear")
+    expect_equal(nrow(result), length(coef(model_fit)))
+  }
+)
+
+# Test case 3: Test the output data frame has the correct number of columns
+test_that(
+  "robust_se returns a data frame with the correct number of columns",
+  {
+    result <- robust_se(model_fit, varcov, scale = "linear")
+    expect_equal(ncol(result), 4)
+  }
+)
+
+# Test case 4: Test that the function returns an error when there is a negative variance
+test_that(
+  "robust_se returns an error when there is a negative variance",
+  {
+    varcov_neg <- varcov
+    diag(varcov_neg)[1] <- -1
+    expect_error(robust_se(model_fit, varcov_neg, scale = "linear"), "Negative variance detected")
+  }
+)
+
+
+# Test case 5: Test that the function works correctly for both "linear" and "log" scales
+test_that(
+  "robust_se works correctly for both linear and log scales",
+  {
+    result_linear <- robust_se(model_fit, varcov, scale = "linear")
+    result_log <- robust_se(model_fit, varcov, scale = "log")
+
+    # Check that the rate ratios are exponentiated in the linear scale
+    expect_equal(result_linear$`Rate ratio`, unname(round(exp(coef(model_fit)), 3)))
+
+    # Check that the rate ratios are not exponentiated in the log scale
+    expect_equal(result_log$`Rate ratio`, unname(round(coef(model_fit), 3)))
+  }
+)
